156b8ca9102958a953894fc17f38c993e367f3dc
Joshua Murphy <joshuamurphy71@gmail.com>
2018-08-21 13:31:59 -0230

    Containers now working with nginx

--- first_run.sh
+++ first_run.sh
@@ -4,6 +4,7 @@
 # Date: May 23rd, 2018
 # https://github.com/toobigtoignore/issf
 
-docker-compose up -d postgres
-./scripts/wait-for-it.sh --strict localhost:5432 -- sleep 30 && ./scripts/postgres_setup.sh
+docker-compose up -d db 
+./scripts/wait-for-it.sh --strict localhost:5432 -- echo "Waiting 60 seconds
+for postgres..." && sleep 60 && ./scripts/postgres_setup.sh
 docker-compose up -d
