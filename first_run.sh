5fe15a10a0c527602a4b3696b4a837f24eea25e3
Jack Arthur Harrhy <me@jackharrhy.com>
2018-12-04 22:39:46 -0330

    Large cleanup
    Misc. Style
    - Trailing spaces removed throughout many files
    - Not none-or-more-than-one new lines at ends of files
    - .editorconfig added with common language specific settings
    
    Frontend
    - Remove random console logs
    - Remove commented out code to attempt to silence the console logs
    - Add new Twitter feed
    - Use HJ-x.js non-min instead of min
    - Remove HJ-x.js backups
    
    Backend
    - Change openstreemap url from http -> https
    - Removed large block of code, since we use git why keep everything?
    - Make email confirmation message a bit nicer
    
    NPM
    - Remove NPM script file
    - Removed all dev deps from package.json, keeping referenced defs
    
    Docker
    - MAINTAINER has been depreciated, removed from Dockerfile

--- first_run.sh
+++ first_run.sh
@@ -4,10 +4,10 @@
 # Date: May 23rd, 2018
 # https://github.com/toobigtoignore/issf
 
-docker-compose up -d db 
+docker-compose up -d db
 ./scripts/wait-for-it.sh --strict localhost:5432 -- echo "Waiting 60 seconds
 for Postgres container..." && sleep 60 && ./scripts/postgres_setup.sh
-docker-compose up -d 
+docker-compose up -d
 
 docker-compose exec -T web /bin/bash -c 'npm install'
 docker-compose exec -T web /bin/bash -c 'virtualenv ../issf'
