cb180cbf244ffc496b9b9a4afbafb2d9421ef4ff
Mitchell Hynes <mitchell.hynes@ecumene.xyz>
2019-07-10 23:56:11 -0230

    Additional field type for horsepower

--- data/postgres/patches/patch_add_range.sql
+++ data/postgres/patches/patch_add_range.sql
@@ -1,5 +1,5 @@
 /* Apply required fields to have range */
-UPDATE public.attribute SET attribute_type='Ordinal', attribute_label='Weakest and most powerful motor', additional_field='Horsepower' WHERE question_number='6C';
+UPDATE public.attribute SET attribute_type='Ordinal', attribute_label='Weakest and most powerful motor', additional_field='Horsepower', additional_field_type='Integer' WHERE question_number='6C';
 INSERT INTO public.attribute_value(attribute_id, value_label, value_order) VALUES (3, 'None in Use', 1), 
                                                                                   (3, 'Not applicable', 2),
                                                                                   (3, 'Gas or Electric Engines', 3),
