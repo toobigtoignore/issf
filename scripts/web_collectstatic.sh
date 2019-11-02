c5532db44ab75dad2f66f231e428fda7e592b592
Tim Carew <tvc802@mun.ca>
2019-11-02 11:37:48 -0230

    make script file runnable, make notify script macos happy

--- scripts/web_collectstatic.sh
+++ scripts/web_collectstatic.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-while true; do
-    inotifywait -r static_root/ && docker-compose exec -T web \
-    /bin/sh -c 'python manage.py collectstatic --noinput'
-done
+
+if [ -x "$(command -v inotifywait)" ]; then
+    while true; do
+        inotifywait -r static_root/ && docker-compose exec -T web \
+        /bin/sh -c 'python manage.py collectstatic --noinput'
+    done
+    exit 0
+fi
+
+if [ -x "$(command -v fswatch)" ]; then
+    fswatch -o ./static_root | xargs -n1 -I{}  docker-compose exec -T web /bin/sh -c 'python manage.py collectstatic --noinput'
+    exit 0
+fi
+
+printf "either fswatch or inotifywait is not installed!"
+exit 1
