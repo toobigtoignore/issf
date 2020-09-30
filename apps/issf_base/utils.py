79fb05ee05d072b56d073d726391f40d5de5922e
Marcos Macedo <marcosmacedo30@hotmail.com>
2020-09-30 13:19:18 -0230

    Fix capacity URL

--- apps/issf_base/utils.py
+++ apps/issf_base/utils.py
@@ -14,6 +14,7 @@ def get_redirectname(core_record_type: str) -> str:
         "SSF Guidelines": 'guidelines-details',
         "Case Study": 'case-studies-details',
         "SSF Experiences": 'experiences-details',
-        "SSF Blue Justice": 'bluejustice-details'
+        "SSF Blue Justice": 'bluejustice-details',
+        "Capacity Development" : 'capacity-details'
     }
     return urls[core_record_type]
